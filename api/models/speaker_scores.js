85a358dd156c7c58381b7d8c46919eec31de7e8d
Nikhila Dharmaji <nikhila.dharmaji@gmail.com>
2019-03-06 01:01:51 +0530

    Create speaker_scores.js

---
+++ api/models/speaker_scores.js
@@ -0,0 +1,23 @@
+/**
+ * speaker_scores.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+	attributes: {
+   speakerID: {
+      model: 'speaker'
+    },
+    roundID: {
+      model: 'rounds'
+    },
+    score: {
+      type:'number'
+    }
+    
+  },
+
+};
+
