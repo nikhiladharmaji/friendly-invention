fdbb2f2f9bb380b7c472767c2239fa11260517ab
Sparshith Sampath <sparshith.sampath1@zomato.com>
2019-03-24 18:50:16 +0530

    Adding an API for adding new team

--- config/policies.js
+++ config/policies.js
@@ -14,6 +14,7 @@ module.exports.policies = {
 
   // Bypass the `is-logged-in` policy for:
   'entrance/*': true,
+  'teams/*': true,
   'account/logout': true,
   'view-homepage-or-redirect': true,
   'deliver-contact-form-message': true,
