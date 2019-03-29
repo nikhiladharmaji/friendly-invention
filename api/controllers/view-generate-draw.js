a2f20d71275b4a4686a03c3843628a58b89cddfc
Sparshith Sampath <sparshith.sampath1@zomato.com>
2019-03-29 10:22:17 +0530

    adding dashboard to generate round

---
+++ api/controllers/view-generate-draw.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View generate draw',
+
+
+  description: 'Display "Generate draw" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/generate-draw'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
