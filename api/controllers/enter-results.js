38ee476bb859e8763734902c37fa46ca12b43561
Sparshith Sampath <sparshith.sampath1@zomato.com>
2019-04-04 20:27:40 +0530

    adding empty controller for enter results

---
+++ api/controllers/enter-results.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Enter results',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
