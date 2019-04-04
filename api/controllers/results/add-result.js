dcf0a3ac909a6457b436fa7664ac2350db58f635
Nikhila Dharmaji <nikhila.dharmaji@gmail.com>
2019-04-04 20:24:25 +0530

    results

---
+++ api/controllers/results/add-result.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Add result',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
